a61bd403ca11220b07acf5584a7a62682559e48c
Oblivion-2k <arnnab.bipul55@gmail.com>
2026-01-19 16:43:28 +0530

    collection framework started

---
+++ src/com/codepractice/corejavacollectionsframeworks/Main.java
@@ -0,0 +1,17 @@
+package com.codepractice.corejavacollectionsframeworks;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        //  int[] array1 = new int[10]; //The syntax to declare an array of size 10 elements. But the size is fixed to 10 and cannot me increased.
+        ArrayList <Integer> arrayList1 = new ArrayList<Integer>(); //Using ArrayList to create and list of elements and perform operation using the methods defined in it.
+        arrayList1.add(5);
+        arrayList1.add(5);
+        arrayList1.add(5);
+        System.out.println(arrayList1);
+
+
+
+
+    }
+}
