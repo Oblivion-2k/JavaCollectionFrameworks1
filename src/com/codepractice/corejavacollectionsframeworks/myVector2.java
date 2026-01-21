8348415288152f26701d3733cec2b2629bf9c56f
Oblivion-2k <arnnab.bipul55@gmail.com>
2026-01-21 22:25:55 +0530

    vector2 class initiated

---
+++ src/com/codepractice/corejavacollectionsframeworks/myVector2.java
@@ -0,0 +1,7 @@
+package com.codepractice.corejavacollectionsframeworks;
+
+public class myVector2 {
+    public static void main(String[] args) {
+        System.out.println("This is myVector2 class");
+    }
+}
