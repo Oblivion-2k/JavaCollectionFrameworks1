077211a123af914fbe88b53c56b3118f1e73c735
Oblivion-2k <arnnab.bipul55@gmail.com>
2026-01-21 22:24:01 +0530

    collection framework started and Vector initiated

---
+++ src/com/codepractice/corejavacollectionsframeworks/myVector.java
@@ -0,0 +1,15 @@
+package com.codepractice.corejavacollectionsframeworks;
+
+import java.util.Vector;
+
+public class myVector {
+    public static void main(String[] args) {
+        Vector<Integer> vector1 = new Vector<>();
+        vector1.add(1);
+        vector1.add(1);
+        vector1.add(1);
+        vector1.add(1);
+        vector1.add(1);
+    }
+
+}
